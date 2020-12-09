5b71d337b039242fc5fdf4dc8b560b6be678ec17
Alex Klyde <alexeykalayda@gmail.com>
2020-12-09 17:06:26 +0200

    fixed

--- lesson8/home4/index.js
+++ lesson8/home4/index.js
@@ -9,3 +9,10 @@ const addPropertyV2 = (obj, key, value) => {
   return Object.assign(obj, { [key]: value });
 }
 
+const addPropertyV3 = (obj, key, value) => {
+  return Object.assign({}, obj, { [key]: value });
+}
+
+const addPropertyV4 = (obj, key, value) => {
+  return {...obj, [key]: value};
+}
