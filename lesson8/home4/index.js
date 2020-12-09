997a9093ce183a7937495fb22beed1328380ea2a
Alex Klyde <alexeykalayda@gmail.com>
2020-12-09 16:51:06 +0200

    commit

--- lesson8/home4/index.js
+++ lesson8/home4/index.js
@@ -1,20 +1,11 @@
 'use strict';
 
-// const user = {
-//   age: 17,
-// }
-
 const addPropertyV1 = (obj, key, value) => { 
   obj[key] = value;
   return obj;
 }
 
-// console.log(addPropertyV1(user, 'name', 'Alex'));
-
-// const user = {}
-
 const addPropertyV2 = (obj, key, value) => {
   return Object.assign(obj, { [key]: value });
 }
 
-console.log(addPropertyV2(user, 'name', 'Alex'));
