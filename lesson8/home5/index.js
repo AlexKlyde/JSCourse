8d0b3102eede91237935fdb480d3fcea25ed2f2c
Alex Klyde <alexeykalayda@gmail.com>
2020-12-09 20:05:24 +0200

    home5

--- lesson8/home5/index.js
+++ lesson8/home5/index.js
@@ -1,20 +1,51 @@
-/*Перебор свойств объектов
-Создайте функцию, которая складывает все значения свойств объектов в массив
+/*Merge objects
+Напишите 4 ф-ции, которые будут мержить (сливать) один объединять с другим
 
 Основные требования:
-Функция с именем concatProps должна принимать объект, как единственный аргумент
-Функция должна достать все значения свойств объекта и сложить их в одну строку ({ name: 'John Doe', age: 17, interest: 'football' } => ['John Doe', 17, 'football'])
-Для пустого объекта ф-ция должна вернуть пустой массив
+Все ф-ции должны быть в файле index.js
+Все ф-ции принимают 2 аргумента: obj1 - объект, obj2 - объект
+Ф-ции должны создать один объект, в котором будут все свойства из первого и второго объекта
+Ф-ция mergeObjectsV1 должна слить объекты так, что в приоритете должны быть свойства из ВТОРОГО объекта. То есть, если в obj1 и obj2 есть свойства с одинаковыми ключами, то в результат должно попасть свойство из obj2. Используйте Object.assign для слияния объектов
+Ф-ция mergeObjectsV2 должна слить объекты так, что в приоритете должны быть свойства из ПЕРВОГО объекта. То есть, если в obj1 и obj2 есть свойства с одинаковыми ключами, то в результат должно попасть свойство из obj1. Используйте Object.assign для слияния объектов
+Ф-ция mergeObjectsV3 должна слить объекты так, что в приоритете должны быть свойства из ВТОРОГО объекта. То есть, если в obj1 и obj2 есть свойства с одинаковыми ключами, то в результат должно попасть свойство из obj2. Используйте spread operator для слияния объектов
+Ф-ция mergeObjectsV4 должна слить объекты так, что в приоритете должны быть свойства из ПЕРВОГО объекта. То есть, если в obj1 и obj2 есть свойства с одинаковыми ключами, то в результат должно попасть свойство из obj1. Используйте spread operator для слияния объектов
+В любом случае объекты obj1 и obj2 должны остаться неизменными
+Все ф-ции должны вернуть итоговый объект
+
 */
+// const obj1 = {
+//   name: 'Tom',
+//   age: 17,
+// };
 
-const concatProps = obj => {
-    let arr = [];
+// const obj2 = {
+//   name: 'Bob',
+//   student: false,
+// };
 
-    for (let key in obj) {
-        arr.push(obj[key]);
-    }
-
-    return arr;
+function mergeObjectsV1(obj1, obj2) {
+  return Object.assign(obj1, obj2);
 }
+// console.log(mergeObjectsV1(obj1, obj2));
+// { name: 'Bob', age: 17, student: false }
 
-console.log(concatProps({ name: 'John Doe', age: 17, interest: 'football' }));
+
+function mergeObjectsV2(obj1, obj2) {
+  return Object.assign(obj2, obj1);
+}
+// console.log(mergeObjectsV2(obj1, obj2))
+//Output: { name: 'Tom', age: 17, student: false }
+
+
+function mergeObjectsV3(obj1, obj2) {
+  return { ...obj1, ...obj2 };
+}
+// console.log(mergeObjectsV3(obj1, obj2))
+// //Output: { name: 'Bob', age: 17, student: false }
+
+
+function mergeObjectsV4(obj1, obj2) {
+  return { ...obj2, ...obj1 };
+}
+// console.log(mergeObjectsV4(obj1, obj2))
+// //Output: { name: 'Tom', age: 17, student: false }
