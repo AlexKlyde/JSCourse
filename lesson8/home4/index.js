1ff98c3de041e46639c39dc5b39238123ab9b80d
Alex Klyde <alexeykalayda@gmail.com>
2020-12-09 17:10:53 +0200

    fixed

--- lesson8/home4/index.js
+++ lesson8/home4/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const addPropertyV1 = (obj, key, value) => { 
+function addPropertyV1(obj, key, value){ 
   obj[key] = value;
   return obj;
 }
 
-const addPropertyV2 = (obj, key, value) => {
+function addPropertyV2(obj, key, value) {
   return Object.assign(obj, { [key]: value });
 }
 
-const addPropertyV3 = (obj, key, value) => {
+function addPropertyV3(obj, key, value) {
   return Object.assign({}, obj, { [key]: value });
 }
 
-const addPropertyV4 = (obj, key, value) => {
+function addPropertyV4(obj, key, value) {
   return {...obj, [key]: value};
 }
