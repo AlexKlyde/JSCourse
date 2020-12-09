cb1bec40004dd0ac9d486b398c50889a8f958be3
Alex Klyde <alexeykalayda@gmail.com>
2020-12-09 16:48:29 +0200

    fixed

--- lesson8/home4/index.js
+++ lesson8/home4/index.js
@@ -1,8 +1,20 @@
-const addPropertyV1 = (obj, key, value) => {
-  return obj[key] = value;
+'use strict';
+
+// const user = {
+//   age: 17,
+// }
+
+const addPropertyV1 = (obj, key, value) => { 
+  obj[key] = value;
+  return obj;
 }
 
+// console.log(addPropertyV1(user, 'name', 'Alex'));
 
+// const user = {}
 
+const addPropertyV2 = (obj, key, value) => {
+  return Object.assign(obj, { [key]: value });
+}
 
-
+console.log(addPropertyV2(user, 'name', 'Alex'));
