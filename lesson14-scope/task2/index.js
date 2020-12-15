a32b11238aff0f59dcf9b74a5b60796d79d4e791
Alex Klyde <alexeykalayda@gmail.com>
2020-12-15 16:34:38 +0200

    Added lesson14

--- lesson14-scope/task2/index.js
+++ lesson14-scope/task2/index.js
@@ -1,19 +1,28 @@
 'use strict';
 
-const createMessenger = () => {
-
+function createMessenger() {
+  let message;
+  let sender;
   function sendMessage(name) {
-    const sender = 'Gromecode';
-
-    console.log(`${name}, ${message}. You ${sender}`);
+    console.log(`${name}, ${message}. Your ${sender}`);
   }
 
   function setMessage(text) {
     message = text;
   }
 
+  function setSender(text) {
+    sender = text;
+  }
+
   return {
     sendMessage,
     setMessage,
+    setSender,
   };
 }
+const Messenger = createMessenger();
+Messenger.setMessage('Just do it');
+Messenger.setSender('Alex Klyde');
+Messenger.sendMessage('Ann');
+
