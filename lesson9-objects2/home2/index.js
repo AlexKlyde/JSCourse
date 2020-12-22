f88dcef036dd37c7b00be8cfc42e73a9cfc90589
Alex Klyde <alexeykalayda@gmail.com>
2020-12-22 11:46:43 +0200

    refactored

--- lesson9-objects2/home2/index.js
+++ lesson9-objects2/home2/index.js
@@ -1,31 +1,25 @@
-/*Посчитать баланс по всем транзакциям
+/* Посчитать баланс по всем транзакциям
 Задан массив транзакций. Нужно посчитать итоговую сумму транзакций
 
 Основные требования:
 Ф-ция должна называться getTotalRevenue и находиться в файле index.js
-Пример работы
-            const dayTransactions = [
-                { userId: 22, amount: 60, operation: 'sell' },
-                { userId: 22, amount: 160, operation: 'buy' },
-                { userId: 44, amount: 90, operation: 'sell' },
-            ];
-            const result = getTotalBalance(dayTransactions);
-            // result = 310
-        
-getTotalRevenue принимает массив транзакций в первом аргументе
-getTotalRevenue должна вернуть число - сумму всех транзакций
-*/
+Пример работы */
 
-// const dayTransactions = [
-//                 { userId: 22, amount: 60, operation: 'sell' },
-//                 { userId: 22, amount: 160, operation: 'buy' },
-//                 { userId: 44, amount: 90, operation: 'sell' },
-//             ];
+const dayTransactions = [
+  { userId: 22, amount: 60, operation: 'sell' },
+  { userId: 22, amount: 160, operation: 'buy' },
+  { userId: 44, amount: 90, operation: 'sell' },
+];
+
+// const result = getTotalBalance(dayTransactions);
+// result = 310;
+
+// getTotalRevenue принимает массив транзакций в первом аргументе
+// getTotalRevenue должна вернуть число - сумму всех транзакций
 
 // input: arr
 // output: number
-const getTotalRevenue = arr => {
-  return arr.reduce((a, b) => a + b.amount, 0);
-}
+const getTotalRevenue = arr =>
+  arr.reduce((acc, el) => acc + el.amount, 0);
 
-// console.log(getTotalRevenue(dayTransactions));
+console.log(getTotalRevenue(dayTransactions));
