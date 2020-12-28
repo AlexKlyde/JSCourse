e066bc9a01d310524233b45ff22802ff00cc9edd
Alex Klyde <alexeykalayda@gmail.com>
2020-12-28 13:13:25 +0200

    fixed

--- lesson23-events-forms-deligation/home/index.js
+++ lesson23-events-forms-deligation/home/index.js
@@ -66,7 +66,7 @@ createBtn.addEventListener('click', addTask);
 
 // Algo
 // 1. add eventListener to List 
-// 2. find the element that constains class .list__item-checkbox
+// 2. find the element that contains class .list__item-checkbox
 // 3. get an id of the target element
 // 4. set up a value of the ckeckbox to the task;
 // 5. update the list
