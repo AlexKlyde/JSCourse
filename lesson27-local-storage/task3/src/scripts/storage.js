daaedffa86ea9d30a3cead2db8512c0dd585f91f
Alex Klyde <alexeykalayda@gmail.com>
2020-12-30 08:21:01 +0200

    fixed

--- lesson27-local-storage/task3/src/scripts/storage.js
+++ lesson27-local-storage/task3/src/scripts/storage.js
@@ -1,5 +1,15 @@
+import { renderTasks } from './renderer.js';
+
 export const setItem = (key, value) => {
   localStorage.setItem(key, JSON.stringify(value));
 };
 
 export const getItem = key => JSON.parse(localStorage.getItem(key));
+
+const onStorageChange = e => {
+  if (e.key === 'tasksList') {
+    renderTasks();
+  }
+};
+
+window.addEventListener('storage', onStorageChange);
