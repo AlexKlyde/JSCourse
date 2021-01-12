a6a5c4903ba9b95dd50488e16ce7af0e96b12079
Alex Klyde <alexeykalayda@gmail.com>
2021-01-12 11:19:07 +0200

    lesson 34 home 2 added

---
+++ lesson34-http-part2/home2/index.js
@@ -0,0 +1,50 @@
+const baseUrl = 'https://5ff9ad2b17386d0017b51ffd.mockapi.io/api/v1/form';
+
+const formElem = document.querySelector('.login-form');
+const errorElem = document.querySelector('.error-text');
+const inputElems = document.querySelectorAll('.form-input');
+const submitBtn = document.querySelector('.submit-button');
+
+const fetchUserData = formData =>
+  fetch(baseUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json;charset=utf-8',
+    },
+    body: JSON.stringify(formData),
+  });
+
+
+const onValidForm = () => {
+  errorElem.textContent = '';
+  
+  if (formElem.reportValidity()) {
+    submitBtn.removeAttribute('disabled');
+  } else {
+    submitBtn.setAttribute('disabled', 'true');
+  }
+}
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const formData = Object.fromEntries(new FormData(formElem));
+  const error = new Error('Failed to create user');
+
+  fetchUserData(formData)
+    .then(response =>
+      response.ok ? response.json() : Promise.reject(response),
+    )
+    .then(result => alert(JSON.stringify(result)))
+    .catch(() => {
+      errorElem.textContent = error;
+    });
+
+  inputElems.forEach(item => {
+    item.value = '';
+  });
+};;
+
+
+formElem.addEventListener('submit', onFormSubmit);
+formElem.addEventListener('input', onValidForm);
+
